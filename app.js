246e2ae2f105ec45c0c80a4a01cccb0a80d2c4d4
Alexey Kryvets <alex.kryvets@gmail.com>
2015-10-21 12:12:03 +0300

    dev

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ angular.module('app', ['tsToast']);
 angular.module('app').config(AppConfig);
 
 function AppConfig (tsToastProvider) {
-    tsToastProvider.configure("main", {
-        verticalAlign: "bottom",
-        horizontalAlign: "center"
-    }, {
-        theme: 'green'
-    });
-    tsToastProvider.configure("one", {
+    tsToastProvider.configure("left-top", {
         verticalAlign: "top",
         horizontalAlign: "left"
     }, {
+        theme: 'green'
+    });
+    tsToastProvider.configure("right-top", {
+        verticalAlign: "top",
+        horizontalAlign: "right"
+    }, {
         showCloseButton: false,
         closeOnClick: true
+    });
+    tsToastProvider.configure("center-bottom", {
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+    }, {
+        theme: 'red'
     });
 }
 AppConfig.$inject = ['tsToastProvider'];
@@ -23,18 +29,33 @@ AppConfig.$inject = ['tsToastProvider'];
 angular.module('app').controller('AppController', AppController);
 
 function AppController ($scope, tsToast) {
-    var oneToast = tsToast("one");
-    var mainToast = tsToast("main");
+    var ltToast = tsToast("left-top");
+    var rbToast = tsToast("right-top");
+    var cbToast = tsToast("center-bottom");
+
+    ltToast.addMessage({title: "Hello", text: "oneyuiyuiyuiy"});
+    ltToast.addMessage({title: "Hello", text: "asdadasdasdadadadad", theme: ""});
+    ltToast.addMessage({title: "Hello", text: "adsadasdddddddddddddddddddddddddada", theme: "red"});
+
+    rbToast.addMessage({title: "Hello", text: "oneyuiyuiyuiy"});
+    rbToast.addMessage({title: "Hello", text: "asdadasdasdadadadad", theme: ""});
+    rbToast.addMessage({title: "Hello", text: "adsadasdddddddddddddddddddddddddada", theme: "red"});
+
+    cbToast.addMessage({title: "Hello", text: "oneyuiyuiyuiy"});
+    cbToast.addMessage({title: "Hello", text: "asdadasdasdadadadad", theme: ""});
+    cbToast.addMessage({title: "Hello", text: "adsadasdddddddddddddddddddddddddada", theme: "red"});
+
     var i = 1;
     $scope.onOpenToastClick = function () {
-        oneToast.addMessage({title: "Hello", text: "oneyuiyuiyuiy" + i});
-        mainToast.addMessage({title: "Hello", text: "mainyuiyuiyiyui"+ i});
+        ltToast.addMessage({title: "Hello", text: "oneyuiyuiyuiy" + i});
+        rbToast.addMessage({title: "Hello", text: "mainyuiyuiyiyui"+ i});
         i++;
     };
 
     $scope.onRemoveAllClick = function () {
-        oneToast.removeAllMessages();
-        mainToast.removeAllMessages();
+        ltToast.removeAllMessages();
+        rbToast.removeAllMessages();
+        cbToast.removeAllMessages();
     };
 }
 AppController.$inject = ['$scope', 'tsToast'];
